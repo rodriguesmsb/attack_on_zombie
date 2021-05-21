22a984235be927a2df2925e0c2aabe00d8aa6cfb
rodriguemsb <rodriguesmsb@gmail.com>
2021-05-21 13:04:06 -0400

    add hero

---
+++ characters.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Thu Apr 15 2021
+@author: Moreno rodrigues rodriguesmsb@gmail.com
+"""
+
+## Import necessary libraries
+import pygame
+from pygame.locals import *
+
+
+class hero(pygame.sprite.Sprite):
+    def __init__(self,x,y, scale):
+        pygame.sprite.Sprite.__init__(self)
+        self.x = x
+        self.y = y
+        self.scale = scale
+        img = pygame.image.load("img/knight/Walk (1).png")
+        self.image = pygame.transform.scale(img, (img.get_width() // self.scale , img.get_height() // self.scale))
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x ,self.y)
+
+
+
+
+            
+
+
---
+++ main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Thu Apr 15 2021
+@author: Moreno rodrigues rodriguesmsb@gmail.com
+"""
+
+## Import necessary libraries
+import pygame
+from pygame.locals import *
+from characters import hero
+
+#initialize pygame
+pygame.init()
+
+#set window dimension
+screen_width = 1000
+screen_height = int(0.8 * screen_width)
+tile_size = 50
+scale = 8
+
+#define fps
+fps = 60
+
+#create window
+screen = pygame.display.set_mode(size = (screen_width, screen_height))
+
+
+#give a title to window
+pygame.display.set_caption("Attack on Zombies")
+
+
+#load background
+background = pygame.image.load("img/graveyard/bg.png")
+
+#create characters
+knight = hero(x = 200, y = 200, scale = 8)
+
+
+def draw_grid():
+	for line in range(0, 20):
+		pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
+		pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+
+
+run = True
+
+
+
+
+while run:
+    screen.blit(source = background, dest = (0,0))
+    screen.blit(source = knight.image, dest = knight.rect)
+    draw_grid()
+
+    #add a way to close the game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    #update the screen
+    pygame.display.update()
+
+
+pygame.quit()
+
